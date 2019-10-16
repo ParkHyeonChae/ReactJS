e01a1ae26f1de5071f6deb8e1a11b31a3ac98b48
ParkHyeonChae <park19996@naver.com>
2019-10-17 01:23:43 +0900

    yts-proxy api사용, movies data 저장

--- Project1/movie_app_2019/src/App.js
+++ Project1/movie_app_2019/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import axios from "axios";
 
 class App extends React.Component{ 
   state = { 
     isLoading: true,
     movies : []
   };
-  componentDidMount(){
-      setTimeout(() => {
-        this.setState({ isLoading: false });
-    }, 6000);
-  }
+  getMovies = async () => {
+      const {
+        data: {
+            data : { movies }
+        }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({ movies, isLoading: false });
+  };
+//   componentDidMount(){
+//       setTimeout(() => {
+//         this.setState({ isLoading: false });
+//     }, 6000);
+//   }
+componentDidMount(){
+    this.getMovies();
+}
   render (){
     const { isLoading } = this.state;
     return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>
