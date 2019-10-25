f4edca0168cf235ec8259175220b426682522182
ParkHyeonChae <park19996@naver.com>
2019-10-25 21:10:01 +0900

    management project 생성 및 Setting

---
+++ Project2/management/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Customer from './component/Customer'
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Customer />
+    );
+  }
+}
+
+export default App;
---
+++ Project2/management/src/component/Customer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Customer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>abc</h2>
+                <p>123</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
